d67016ee4d99cd15a5995a10671610ca800b3798
Johannes Røsvik <j.rosvik@gmail.com>
2024-03-23 21:40:33 +0100

    reexport image types from libimg

--- libimg/src/lib.rs
+++ libimg/src/lib.rs
@@ -1,14 +1,22 @@
-use std::error::Error;
-use std::io::ErrorKind;
+use std::io::{Cursor, Error, ErrorKind};
 
-pub async fn fetch_image(url: String) -> Result<image::DynamicImage, Box<dyn Error>> {
+pub use image::DynamicImage as Image;
+pub use image::ImageOutputFormat as Format;
+
+pub mod format {
+    pub use image::ImageOutputFormat::Gif;
+    pub use image::ImageOutputFormat::Jpeg;
+    pub use image::ImageOutputFormat::Png;
+}
+
+pub async fn fetch_image(url: String) -> Result<image::DynamicImage, Box<dyn std::error::Error>> {
     let image_response = match reqwest::get(url).await {
         Ok(r) => r,
         Err(e) => return Err(Box::new(e)),
     };
     let status_code = image_response.status();
     if !status_code.is_success() {
-        return Err(Box::new(std::io::Error::new(
+        return Err(Box::new(Error::new(
             ErrorKind::Other,
             format!("Status code from source: {}", status_code),
         )));
